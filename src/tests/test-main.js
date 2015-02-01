3abae025e8ac9fccce04db703e88f0cd63f3d09a
QasimK <QasimK@users.noreply.github.com>
2015-02-01 17:04:34 +0000

    Closes #5 - Automatically load test pages
    Using `cfx test`.

--- src/tests/test-main.js
+++ src/tests/test-main.js
@@ -1,12 +1,40 @@
+"use strict";
+
+var tabs = require("sdk/tabs");
 var main = require("./main");
 
-exports["test main"] = function(assert) {
-  assert.pass("Unit test running!");
-};
-
-exports["test main async"] = function(assert, done) {
-  assert.pass("async Unit test running!");
-  done();
+// Put all tests together so tabs open all at once
+exports["test link hover"] = function(assert, done) {
+  const NUM_PAGES = 3;
+  var num_closed = 0;
+  var doneTest = function(doneMsg) {
+    assert.pass(doneMsg);
+    num_closed += 1;
+    if(num_closed == NUM_PAGES) {
+      done();
+    }
+  };
+  
+  tabs.open({
+    url: "./tests/test-link-hover.html",
+    onClose: function(tab) {
+      doneTest("Test link hover: DONE");
+    }
+  });
+  
+  tabs.open({
+    url: "./tests/test-smooth-mouse.html",
+    onClose: function(tab) {
+      doneTest("Test smooth mouse: DONE");
+    }
+  });
+  
+  tabs.open({
+    url: "./tests/test-image-hover.html",
+    onClose: function(tab) {
+      doneTest("Test image hover: DONE");
+    }
+  });
 };
 
 require("sdk/test").run(exports);
